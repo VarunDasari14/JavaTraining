70cf1359cb6ae3b7292b12cbc7da71918275c06a
Dasari, Varun <vrndasari@gmail.com>
2023-01-22 12:42:44 -0500

    Initial java homework

---
+++ demo/src/com/varun/homework/Calculator.java
@@ -0,0 +1,21 @@
+package com.varun.homework;
+
+public class Calculator {
+	
+	public static String add(String num1, String num2) {
+		return "";
+	}
+	
+	public static String subtract(String num1, String num2) {
+		return "";
+	}
+	
+	public static String multiply(String num1, String num2) {
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Calculator.add("1", "1"));
+	}
+
+}
---
+++ demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mrond
+ *
+ */
+module demo {
+}
